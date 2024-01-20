9f1e1533c90eac18c2709b5a730099c02868ed31
oluwaseun <adunmofestus@gmail.com>
2024-01-20 03:30:31 +0100

    masterclass

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -6,11 +6,13 @@ const Header = () => {
   return (
     <header>
       <div className='wrapper'>
+        <div className='cta'>
         <p className='course-name'>Sound Design MasterClass</p>
         <h1>Learn The Art Of Sound Desgin</h1>
         <a href='#' className='demo-btn'>
           Demo Lesson
           </a>
+          </div>
       </div>
     </header>
   )
