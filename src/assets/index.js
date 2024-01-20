83a80def7694ac0716b75989c62ef8980c4a2aaa
oluwaseun <adunmofestus@gmail.com>
2024-01-20 02:29:29 +0100

    images added

---
+++ src/assets/index.js
@@ -0,0 +1,19 @@
+import close from "./close.webp";
+import console from "./console.avif";
+import dropdown from "./dropdown.png";
+import facebook from "./facebook.png";
+import female from "./female.webp";
+import instagram from "./instagram.png";
+import male from "./male.webp";
+import right from "./right.png";
+import twitter from "./twitter.png";
+import test1 from "./test-1.avif";
+import test2 from "./test-2.avif";
+import test3 from "./test-3.avif";
+import firstbackground from "./first background.webp";
+import female2 from "./female-2.webp";
+import console1 from "./console-1.webp";
+import console2 from "./console-2.avif";
+import console3 from "./console-3.avif";
+
+export {close, console, dropdown, facebook, female, instagram, male, right, twitter,test1, test2, test3, firstbackground, female2, console1, console2, console3}
