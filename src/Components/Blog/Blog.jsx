f60042b9a5e1adc75181eb02dbbc0173707410b4
oluwaseun <adunmofestus@gmail.com>
2024-01-20 16:36:17 +0100

    blog

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import "./Blog.css"
+import {male ,female, female2 } from "../../assets"
 
 const Blog = () => {
   return (
-    <div>
-      blog
-    </div>
+    <section id='blog' className='pink'>
+      <div className='wrapper'>
+        <h2 className='light'>Latest Posts</h2>
+        <div className='content-container'>
+          <div className='post'>
+            <div  className='tag'>Daw</div>
+            <a href='#'><img src={male} alt="" /></a>
+            <a href='#'><h3 className='post-title'>
+              How to Use Drum Machine in Logic pro X</h3></a>
+          </div>
+        </div>
+      </div>
+      
+    </section>
   )
 }
 
