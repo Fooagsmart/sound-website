8f2d1c492f96f77279de71b52cdcaff2ed22cebd
oluwaseun <adunmofestus@gmail.com>
2024-01-20 13:27:51 +0100

    topics

--- src/Components/Topics/Topic.jsx
+++ src/Components/Topics/Topic.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Topic.css"
+import {console,right, console1, console2 , console3} from "../../assets"
 
 const Topic = () => {
+
+  const [ currentImage, setCurrentImage] = useState(console);
   return (
-    <div>
-      topic
-    </div>
+    <section id='topic' className='black'>
+      <div className='wrapper'>
+         <h2>What will you learn?</h2>
+         <div className='content-container'>
+          <ul>
+            <li onMouseEnter={()=> setCurrentImage(console3)}>What are Frequencies</li>
+            <li onMouseEnter={() => setCurrentImage(console2)}>Using the Daw</li>
+            <li onMouseEnter={() => setCurrentImage(console3)}>Vocals Processing</li>
+            <li onMouseEnter={() => setCurrentImage(console2)}>Mixing</li>
+            <li onMouseEnter={() => setCurrentImage(console1)}>mixing Console</li>
+            <li onMouseEnter={() => setCurrentImage(console)}>Mastering</li>
+          </ul>
+
+          <div className='topic-image'>
+            <img src={currentImage}></img>
+          </div>
+         </div>
+         <img src={right} className='right'></img>
+      </div>
+    </section>
   )
 }
 
--- src/assets/index.js
+++ src/assets/index.js
@@ -7,14 +7,14 @@ import instagram from "./instagram.png";
 import male from "./male.webp";
 import right from "./right.png";
 import twitter from "./twitter.png";
-import test1 from "./test-1.avif";
-import test2 from "./test-2.avif";
-import test3 from "./test-3.avif";
-import firstbackground from "./first background.webp";
-import female2 from "./female-2.webp";
-import console1 from "./console-1.webp";
-import console2 from "./console-2.avif";
-import console3 from "./console-3.avif";
+import test1 from "./test1.avif";
+import test2 from "./test2.avif";
+import test3 from "./test3.avif";
+import firstbackground from "./firstbackground.webp";
+import female2 from "./female2.webp";
+import console1 from "./console1.webp";
+import console2 from "./console2.avif";
+import console3 from "./console3.avif";
 
 export {
   close,
