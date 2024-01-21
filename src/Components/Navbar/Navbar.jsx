4b90fb5323e3798036537b5d8ad07f565ed56ff8
oluwaseun <adunmofestus@gmail.com>
2024-01-21 22:36:19 +0100

    onclick function

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,31 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./Navbar.css"
 import {dropdown, close} from "../../assets"
 
 const Navbar = () => {
+
+  const [isActive, setIsActive] = useState (false);
   return (
    <> <nav>
       <a href='#' className='logo'>Sound<span className='red'>DZign</span></a>
 
       <ul>
-        <li><a href='#topic' className='about'>About</a></li>
-        <li><a href='#info' className='detail'>Course Details</a></li>
+      <li><a href='#topic' className='detail'>Course Details</a></li>
+        <li><a href='#info' className='about'>About</a></li>
         <li><a href='#blog' className='blog'>Blog</a></li>
         <li><a href='#testimonial' className='people'>Testimonials</a></li>
       </ul>
 
-      <div className='menu-icon'>
+      <div onClick={() => setIsActive(true)} className='menu-icon'>
         <img src={dropdown} alt="" />
       </div>
     </nav>
 
-    <div className='mobile-menu-container'>
-      <div className='close-icon'>
+    <div className={`mobile-menu-container ${isActive ? "active" : ""}`}>
+      <div onClick={() => setIsActive(false)} className='close-icon'>
         <img src={close} alt="" />
       </div>
-      <ul className='menu-item'>
-      <li><a href='#info' className='detail'>Course Details</a></li>
-        <li><a href='#topic' className='about'>About</a></li>
+      <ul className='menu-item '>
+      <li><a href='#topic' className='detail'>Course Details</a></li>
+        <li><a href='#info' className='about'>About</a></li>
         <li><a href='#blog' className='blog'>Blog</a></li>
         <li><a href='#testimonial' className='people'>Testimonials</a></li>
       </ul>
