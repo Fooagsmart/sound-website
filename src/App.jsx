f89b1e108927a1ae57cc8ab37623f863c7116a13
oluwaseun <adunmofestus@gmail.com>
2024-01-22 05:58:56 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { Header, Info, Testimonial, Topic, Blog, Footer,} from './Components'
-import client from "./client";
+
 
 const App = () => {
 
-  const [post, setPost] = useState ([]);
-
-  useEffect(() => {
-    const query = '*[_type == "post"]'; 
-
-    client.fetch(query).then((data) => {
-      setPost(data);
-    })
-  }, [])
     return (
     <>
       <Header />
-      <h1>{post[1] && post[1].title}</h1>
       <Topic />
       <Info />
       <Blog />
